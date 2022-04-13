bbf7669853f1d9bd891a482d4c7ae1a96cc1151d
joris-lefondeur <joris.lefondeur.001@student.uni.lu>
2022-04-14 00:06:04 +0200

    Add transitions between all scenes with buttons in game

--- pokeprof/Assets/Scripts/SceneTransition.cs
+++ pokeprof/Assets/Scripts/SceneTransition.cs
@@ -12,6 +12,8 @@ public class SceneTransition : MonoBehaviour
 
     public bool pressed = false;
 
+    public string sceneName;
+
     public void isPressed() {
         pressed = true;
     }
@@ -19,17 +21,54 @@ public class SceneTransition : MonoBehaviour
     void Update()
     {   
         if(pressed) {
-        StartCoroutine(LoadScene());
+        StartCoroutine(LoadScene(sceneName));
         }
     }
 
-    IEnumerator LoadScene(){
+    IEnumerator LoadScene(string sceneName){
         transitionAnim.SetTrigger("end");
         yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame() {
         Application.Quit();
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.EventSystems;
+// using UnityEngine.UI;
+
+// public class SceneTransition : MonoBehaviour
+// {
+
+//     public Animator transitionAnim;
+
+//     public bool pressed = false;
+
+//     public void isPressed() {
+//         pressed = true;
+//     }
+
+//     void Update()
+//     {   
+//         if(pressed) {
+//         StartCoroutine(LoadScene());
+//         }
+//     }
+
+//     IEnumerator LoadScene(){
+//         transitionAnim.SetTrigger("end");
+//         yield return new WaitForSeconds(1.5f);
+//         SceneManager.LoadScene("Game");
+//     }
+
+//     public void QuitGame() {
+//         Application.Quit();
+//     }
+// }
+
