25ea9b15b7df30b48e0a7c3f0a6de2d25c22eacf
joris-lefondeur <joris.lefondeur.001@student.uni.lu>
2022-04-14 19:32:47 +0200

    Add settings menu and transitions

--- pokeprof/Assets/Scripts/SceneTransition.cs
+++ pokeprof/Assets/Scripts/SceneTransition.cs
@@ -36,39 +36,3 @@ public class SceneTransition : MonoBehaviour
     }
 }
 
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.EventSystems;
-// using UnityEngine.UI;
-
-// public class SceneTransition : MonoBehaviour
-// {
-
-//     public Animator transitionAnim;
-
-//     public bool pressed = false;
-
-//     public void isPressed() {
-//         pressed = true;
-//     }
-
-//     void Update()
-//     {   
-//         if(pressed) {
-//         StartCoroutine(LoadScene());
-//         }
-//     }
-
-//     IEnumerator LoadScene(){
-//         transitionAnim.SetTrigger("end");
-//         yield return new WaitForSeconds(1.5f);
-//         SceneManager.LoadScene("Game");
-//     }
-
-//     public void QuitGame() {
-//         Application.Quit();
-//     }
-// }
-
