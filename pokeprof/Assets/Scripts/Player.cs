b7d365cb733492d9bf2b8e5bd5f13c69c3539edf
joris-lefondeur <joris.lefondeur.001@student.uni.lu>
2022-04-29 20:47:57 +0200

    Fix player movement issues with joystick

--- pokeprof/Assets/Scripts/Player.cs
+++ pokeprof/Assets/Scripts/Player.cs
@@ -6,7 +6,6 @@ public class Player : MonoBehaviour
 {
     public MovementJoystick movementJoystick;
     public float playerSpeed;
-    private Rigidbody2D rb;
     private bool playerMoving;
     private Vector2 lastMove;
 
@@ -15,7 +14,6 @@ public class Player : MonoBehaviour
     void Start()
 
     {
-        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -24,25 +22,19 @@ public class Player : MonoBehaviour
         playerMoving = false;
 
         if(movementJoystick.joystickVec.x > 0.5f || movementJoystick.joystickVec.x < -0.5f) {
-            rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
+            transform.Translate(new Vector3(movementJoystick.joystickVec.x * playerSpeed * Time.deltaTime, 0f, 0f));
             playerMoving = true;
-            lastMove = new Vector2(movementJoystick.joystickVec.x, 0f);
+            lastMove = new Vector2(Mathf.Round(movementJoystick.joystickVec.x), 0f);
         }
 
         if(movementJoystick.joystickVec.y > 0.5f || movementJoystick.joystickVec.y < -0.5f) {
-            rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
+            transform.Translate(new Vector3(0f, movementJoystick.joystickVec.y * playerSpeed * Time.deltaTime, 0f));
             playerMoving = true;
-            lastMove = new Vector2(0f, movementJoystick.joystickVec.y);
+            lastMove = new Vector2(0f, Mathf.Round(movementJoystick.joystickVec.y));
         }
-
-        if(movementJoystick.joystickVec.x == 0 || movementJoystick.joystickVec.y == 0) {
-            rb.velocity = Vector2.zero;
-            playerMoving = false;
-        }
-
         
-        anim.SetFloat("MoveX", movementJoystick.joystickVec.x);
-        anim.SetFloat("MoveY", movementJoystick.joystickVec.y);
+        anim.SetFloat("MoveX", Mathf.Round(movementJoystick.joystickVec.x));
+        anim.SetFloat("MoveY", Mathf.Round(movementJoystick.joystickVec.y));
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
