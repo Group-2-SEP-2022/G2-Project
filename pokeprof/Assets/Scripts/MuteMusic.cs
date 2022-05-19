392f54719f65a8a6b82bbf9dc37c17540a4b782d
joris-lefondeur <joris.lefondeur.001@student.uni.lu>
2022-05-19 19:46:18 +0200

    Add background music

---
+++ pokeprof/Assets/Scripts/MuteMusic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteMusic : MonoBehaviour
+{   
+    public GameObject music;   
+    public MusicData musicData;
+
+    void Start()
+    {
+        if(musicData.isOff) {
+            music.SetActive(false);
+        }
+    }
+}
---
+++ pokeprof/Assets/Scripts/Scriptable Object/MusicData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MusicData")]
+public class MusicData : ScriptableObject
+{
+    public bool isOff;
+}
